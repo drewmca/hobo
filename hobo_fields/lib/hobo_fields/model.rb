1490a316034d108fc7a13726255b55a73b7cb8ef
drewmca <drew@unitdev.com>
2015-02-21 19:28:12 -0800

    Update model.rb

--- hobo_fields/lib/hobo_fields/model.rb
+++ hobo_fields/lib/hobo_fields/model.rb
@@ -90,7 +90,7 @@ module HoboFields
       index_options = {}
       index_options[:name] = options.delete(:index) if options.has_key?(:index)
       bt = belongs_to_without_field_declarations(name, options, &block)
-      puts "AVAILABLE REFLECTIONS:"
+      puts "AVAILABLE REFLECTIONS for '#{name}:"
       puts reflections
       refl = reflections[name] || reflections[name.to_sym]
       fkey = refl.foreign_key
